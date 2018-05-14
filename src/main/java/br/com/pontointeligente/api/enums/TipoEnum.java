26069480a7886f7ac6692981f9fbbc0285b4f0aa
silvio <silvio@silvio-Inspiron-3442>
2018-05-14 09:03:15 -0300

    Adição das entidades e enums.

---
+++ src/main/java/br/com/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.pontointeligente.api.enums;
+
+/**
+ *
+ * @author silvio
+ */
+public enum TipoEnum {
+    INICIO_TRABALHO,
+    TERMINO_TRABALHO,
+    INICIO_ALMOCO,
+    TERMINO_ALMOCO,
+    INICIO_PAUSA,
+    TERMINO_PAUSA
+}
